8a735bd1e087e1cbec1bd4193edcc8f2332e4985
Unjuno <unjuno.org@unjuno.org>
2026-06-05 21:41:10 +0900

    ランダム教師スモークテストを完全版にする

--- tests/test_random_teacher_smoke.py
+++ tests/test_random_teacher_smoke.py
@@ -8,4 +8,4 @@ class TestRandomTeacherSmoke(unittest.TestCase):
     def test_random_teacher_has_positive_edge(self):
         teacher = random_teacher_smoke.build_random_teacher(node_count=4, seed=0)
         self.assertTrue(any(value == 1 for value in teacher.values()))
-        self.assertGreaterEqual(run_experiments.utility_proxy(teacher
+        self.assertGreaterEqual(run_experiments.utility
